880e82688fff6b08d63813d5d4abb665ca6e9740
Brume <54155619+brumef2g2@users.noreply.github.com>
2022-08-02 11:08:55 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -55,6 +55,11 @@ resource "aws_instance" "web" {
               echo "Hello World" > /var/www/html/index.html
               systemctl restart apache2
               EOF
+  
+  tags = {
+    name = "DevTeam"
+    enviroment = "production"
+  }
 }
 
 resource "aws_security_group" "web-sg" {
